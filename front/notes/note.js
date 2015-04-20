46305ef6ad8624cc1092565905f5c00cb2865e02
fccardiff <fccardiff@yahoo.com>
2015-04-20 14:50:43 -0400

    fixed issues with loading contacts correctly

--- front/notes/note.js
+++ front/notes/note.js
@@ -1,34 +1,38 @@
-(function() {
+(function () {
     var newNote = document.querySelector('#note-text');
     var saveButton = document.querySelector('#save');
     var notes = document.querySelector('#notes');
     var contacts = document.querySelector('#contacts');
-
     function getContacts() {
         if (!('mozContacts' in navigator)) {
             return;
         }
-
         var req = window.navigator.mozContacts.getAll();
+        req.onsuccess = function () {
 
-        req.onsuccess = function() {
-            if (!this.result) return;
+            if (this.result) {
+                // Display the name of the contact
+                var option = document.createElement('option');
+                console.log(this.result.givenName + ' ' + this.result.familyName);
+                option.textContent = this.result.givenName + ' ' +
+                    this.result.familyName;
+                contacts.appendChild(option);
 
-            var option = document.createElement('option');
-            option.textContent = this.result.givenName + ' ' +
-                                 this.result.familyName;
-            contacts.appendChild(option);
+                // Move to the next contact which will call the request.onsuccess with a new result
+                this.continue();
 
-            this.continue;
+            } else {
+                
+            }
         };
 
-        req.onerror = function() {
+        req.onerror = function () {
             console.error('Could not recieve contacts!', req.error);
         };
     }
     getContacts();
 
-    saveButton.addEventListener('click', function() {
+    saveButton.addEventListener('click', function () {
         function store(location) {
             var newItem = document.createElement('li');
             newItem.textContent = newNote.value;
@@ -43,12 +47,11 @@
             navigator.geolocation.getCurrentPosition(function (position) {
                 store(position.coords.latitude + ', ' +
                     position.coords.longitude);
-            }, function(err) {
+            }, function (err) {
                 console.error('Failed to get user location', err);
                 store('Could not get your location');
             });
-        }
-        else {
+        } else {
             store('You don\'t have GPS');
         }
     });
